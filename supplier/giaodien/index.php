04079b663380ac7510a801133285a8c2182883c0
tranquynhnam <tranquynhnam789@gmail.com>
2024-03-13 15:10:34 +0700

    add bootstrap

--- supplier/giaodien/index.php
+++ supplier/giaodien/index.php
@@ -3,7 +3,11 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./css/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script>src="./css/bootstrap-5.1.3-dist/js/bootstrap.min.js"</script>
 <title>Trang nhà bán</title>
+
 <style>
     body {
         font-family: Arial, sans-serif;
@@ -14,7 +18,6 @@
         background-color: #333;
         color: #fff;
         padding: 10px;
-        text-align: center;
     }
     .left-menu {
         background-color: #f4f4f4;
@@ -61,7 +64,16 @@
 <body>
 
 <header>
-    <h1>TRANG NHÀ BÁN</h1>
+    <div class="row">
+        <div class="col-md-3">
+            <img style="width:125px;" src="../../img/logo.png" alt="">
+        </div>
+        <div class="col-md-9">
+            <h1>TRANG NHÀ BÁN </h1>
+        </div>
+
+    </div>
+
 </header>
 
 <div class="left-menu">
