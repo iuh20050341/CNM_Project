c21c0e93c75bc96a19970c7557cd4c0497be9cf7
tranquynhnam <tranquynhnam789@gmail.com>
2024-05-19 18:04:22 +0700

    rangbuoc

--- frontend/register.php
+++ frontend/register.php
@@ -117,13 +117,14 @@ color: #fff;
         <br><input class="input" type="text" name="ten_dangnhap" value="" required placeholder="Tên đăng nhập" ><br>
         </div>
         <div class="form-group">
-        <br><input class="input" type="password" name="mat_khau" value="" required placeholder="Mật khẩu" /><br>
+        <br><input class="input" type="password" name="mat_khau" value="" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$" 
+       title="Mật khẩu phải chứa từ 8 - 15 ký tự, bao gồm ít nhất một chữ thường, một chữ in hoa, một số và một ký tự đặc biệt" required placeholder="Mật khẩu" /><br>
         </div>
         <div class="form-group">
         <br><input class="input" type="email" name="email" value="" required placeholder="Email" /><br>
         </div>
         <div class="form-group">
-        <br><input class="input" type="text" name="phone" value="" pattern="[0-9]{10}" id="phone" required placeholder="Số điện thoại" /><br>
+        <br><input class="input" type="text" name="phone" value="" id="phone" pattern="0[0-9]{9}" title="Số điện thoại phải bắt đầu bằng số 0 và có đúng 10 ký tự" required placeholder="Số điện thoại" /><br>
         </div>
         <div class="form-group">
         <div class="radio_option">
